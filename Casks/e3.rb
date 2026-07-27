e648219974f5fb111239770e79a7bcdd50638ab1
boyswan <jackwilliamboyce@gmail.com>
2026-07-28 00:48:01 +0100

    e3 0.1.12

--- Casks/e3.rb
+++ Casks/e3.rb
@@ -2,17 +2,17 @@
 # frozen_string_literal: true
 
 cask "e3" do
-  version "0.1.11"
+  version "0.1.12"
 
   on_arm do
-    sha256 "238816bc9db21300a1b4d27fe2fa509d25090db00bc487e16cd807bc51b6aa51"
+    sha256 "7e58100fa8a242c709d23474f159e7d4885e0041167ea9f050498b4652ea19bd"
 
-    url "https://github.com/boyswan/e3/releases/download/v0.1.11/e3-0.1.11-Darwin-arm64-app.zip"
+    url "https://github.com/boyswan/e3/releases/download/v0.1.12/e3-0.1.12-Darwin-arm64-app.zip"
   end
   on_intel do
-    sha256 "11b5ad927ba9f9e07ebe58922d17797f49a75f2e61340027aeb3de6ad5909797"
+    sha256 "290a79489191e8610ed61fa3efcde3bd7ef84dd549045a9059bf3d877afb6372"
 
-    url "https://github.com/boyswan/e3/releases/download/v0.1.11/e3-0.1.11-Darwin-x86_64-app.zip"
+    url "https://github.com/boyswan/e3/releases/download/v0.1.12/e3-0.1.12-Darwin-x86_64-app.zip"
   end
 
   name "e3"
--- Formula/e3-cli.rb
+++ Formula/e3-cli.rb
@@ -5,7 +5,7 @@
 class E3Cli < Formula
   desc "I3-inspired terminal multiplexer"
   homepage "https://github.com/boyswan/e3"
-  version "0.1.11"
+  version "0.1.12"
   license "MIT"
 
   depends_on :macos
@@ -14,13 +14,13 @@ class E3Cli < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/boyswan/e3/releases/download/v0.1.11/e3-0.1.11-Darwin-arm64.tar.gz"
-      sha256 "2a7f3a11bdac1c8f6edb3779275a2a4fd6552b6e7546df9abd7508880bd6fe1a"
+      url "https://github.com/boyswan/e3/releases/download/v0.1.12/e3-0.1.12-Darwin-arm64.tar.gz"
+      sha256 "79d3a1a1b8720b111b0e7d9cf94fcb221cec0b8d3136f5e5c011628b527d185a"
     end
 
     on_intel do
-      url "https://github.com/boyswan/e3/releases/download/v0.1.11/e3-0.1.11-Darwin-x86_64.tar.gz"
-      sha256 "ff536d1513409da29a6d8a6a2909faa0d8e7c441e217bdfd5636337726525b76"
+      url "https://github.com/boyswan/e3/releases/download/v0.1.12/e3-0.1.12-Darwin-x86_64.tar.gz"
+      sha256 "909d2af05d38269519e7510e81cd52c59c973aa00e85ce253b617165bc26bc3f"
     end
   end
 
