a3af69838410cce926d52b204840701311ec99d3
boyswan <jackwilliamboyce@gmail.com>
2026-07-28 18:32:06 +0100

    e3 0.1.20

--- Formula/e3-cli.rb
+++ Formula/e3-cli.rb
@@ -5,7 +5,7 @@
 class E3Cli < Formula
   desc "I3-inspired terminal multiplexer"
   homepage "https://github.com/boyswan/e3"
-  version "0.1.19"
+  version "0.1.20"
   license "MIT"
 
   depends_on :macos
@@ -14,13 +14,13 @@ class E3Cli < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/boyswan/e3/releases/download/v0.1.19/e3-0.1.19-Darwin-arm64.tar.gz"
-      sha256 "7ad39d3de8ca987ec6d81304bf85fc790b30d449eab85a7451cb04dddf30c369"
+      url "https://github.com/boyswan/e3/releases/download/v0.1.20/e3-0.1.20-Darwin-arm64.tar.gz"
+      sha256 "d92fc311dbccd5e515989c18e68e2cdb365b71f126de64e019b798c5c769fdec"
     end
 
     on_intel do
-      url "https://github.com/boyswan/e3/releases/download/v0.1.19/e3-0.1.19-Darwin-x86_64.tar.gz"
-      sha256 "81c1de04e61575d35d2abdeb9bf9ac2d4e2285c4a64d701485b39ce0c31287de"
+      url "https://github.com/boyswan/e3/releases/download/v0.1.20/e3-0.1.20-Darwin-x86_64.tar.gz"
+      sha256 "6f277e1fa84c82214f7f9467e2bdcfa99ade421a569edd0ef4c7afa0562685c6"
     end
   end
 
